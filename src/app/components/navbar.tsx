f69a3e629ea6c0c0def5376c338f531a2053cbbc
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-05-14 01:17:37 +0100

    fix svg variable naming

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -15,7 +15,7 @@ export default function NavBar() {
 
             </nav>
             <button className="text-[var(--night)] inline-flex items-center bg-[var(--ash-grey)] border-0 py-3 px-6 focus:outline-none hover:bg-[var(--timberwolf)] rounded text-base mt-4 md:mt-0">Get Started
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
             </button>
