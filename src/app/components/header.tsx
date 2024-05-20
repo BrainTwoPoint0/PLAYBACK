9ba7fc35af5cb8e7f99a66def4f564ce4a201d2e
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-05-20 21:27:54 +0100

    fix color scheme and remove dark: targeting

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -18,9 +18,9 @@ export function Header() {
                     duration: 0.5,
                     ease: [0.4, 0.0, 0.2, 1],
                 }}
-                className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto ">
+                className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto ">
                 Relive the Play, Capture the Highlight,{" "}
-                <Highlight className="text-black dark:text-white">
+                <Highlight className="text-white">
                     Access the Moment.
                 </Highlight>
             </motion.h1>
