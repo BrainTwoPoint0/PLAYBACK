4457643280ff69e01cac5025d9c115b791a224ce
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-05-20 18:42:12 +0100

    add header (no styling)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,20 @@ import { ParallaxText } from "./components/ParallaxText";
 import Team from "./components/team";
 import Image from "next/image";
 import Venues from "./components/venues";
+import { Header } from "./components/header";
 
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center overflow-hidden">
-
+    <main className="overflow-hidden">
+      <Header />
       <Venues />
-      <Team />
-      <section>
+      <section className="my-20">
         <ParallaxText baseVelocity={3}>Access The Moment</ParallaxText>
         <ParallaxText baseVelocity={-3}>Share The Memories</ParallaxText>
       </section>
+      <Team />
     </main>
   );
 }
