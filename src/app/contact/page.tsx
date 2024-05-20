e26833549749a409d9912759c2c9d3dfa8f40779
fedecech <federicocech@gmail.com>
2024-05-20 18:06:59 +0100

    add contact page and form

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { ContactForm } from '../components/contact';
+
+export default function ContactPage() {
+  return (
+    <main className="min-h-[80vh] flex items-center justify-center">
+      <ContactForm />
+    </main>
+  );
+}
