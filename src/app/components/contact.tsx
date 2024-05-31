a1e5a657fa4e7127660ce5e5b2ca7f380fee0863
fedecech <federicocech@gmail.com>
2024-05-31 16:04:21 +0100

    netlify form

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { Label } from './ui/label';
 import { Input } from './ui/input';
 import { cn } from '@/app/utils/cn';
+import { Textarea } from './ui/textarea';
+import { RadioGroup, RadioGroupItem } from '@/app/components/ui/radio-group';
 
 export function ContactForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -12,16 +14,30 @@ export function ContactForm() {
   };
 
   return (
-    <div className="md:border border-[var(--timberwolf)]  max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-[var(--night)]">
-      <h2 className="font-bold text-xl text-neutral-200">
-        Join us
-      </h2>
+    <div className="md:border border-[var(--timberwolf)]  max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-[var(--night)] mb-8">
+      <h2 className="font-bold text-xl text-neutral-200">Join us</h2>
       <p className=" text-sm max-w-sm mt-2 text-neutral-300">
         Become a PLAYBACK venue partner.
       </p>
 
-      <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+      <form
+        className="my-8 flex flex-col items-start space-y-6"
+        onSubmit={handleSubmit}
+        data-netlify="true"
+        name="contact"
+      >
+        <RadioGroup defaultValue="venue" className="mb-2">
+          <Label className="mb-2">How am I?</Label>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="venue" id="r1" />
+            <Label htmlFor="r1">Venue</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="camera_provider" id="r2" />
+            <Label htmlFor="r2">Camera provider</Label>
+          </div>
+        </RadioGroup>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
             <Input id="firstname" placeholder="Tyler" type="text" />
@@ -31,15 +47,18 @@ export function ContactForm() {
             <Input id="lastname" placeholder="Durden" type="text" />
           </LabelInputContainer>
         </div>
-        <LabelInputContainer className="mb-4">
+        <LabelInputContainer>
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
         </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="password">Company</Label>
-          <Input id="password" placeholder="Alphabet LTD" type="password" />
+        <LabelInputContainer>
+          <Label htmlFor="company">Company</Label>
+          <Input id="company" placeholder="Alphabet LTD" type="text" />
         </LabelInputContainer>
-
+        <LabelInputContainer>
+          <Label htmlFor="message">Message (optional)</Label>
+          <Textarea id="message" placeholder="Hi..." />
+        </LabelInputContainer>
         <button
           className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,50 +1,75 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const svgToDataUri = require("mini-svg-data-uri");
+const svgToDataUri = require('mini-svg-data-uri');
 const {
   default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+} = require('tailwindcss/lib/util/flattenColorPalette');
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
   plugins: [
+    require('tailwindcss-animate'),
     addVariablesForColors,
     function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
-          "bg-dot-thick": (value: any) => ({
+          'bg-dot-thick': (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
             )}")`,
           }),
         },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+        {
+          values: flattenColorPalette(theme('backgroundColor')),
+          type: 'color',
+        }
       );
     },
   ],
 };
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
+  let allColors = flattenColorPalette(theme('colors'));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
   addBase({
-    ":root": newVars,
+    ':root': newVars,
   });
 }
 export default config;
