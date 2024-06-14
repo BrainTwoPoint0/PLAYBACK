33fb03fccc33e5d7250a4b75727b2cb28518b7be
fedecech <federicocech@gmail.com>
2024-06-14 17:25:12 +0100

    fix netlify contact form

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,27 +1,65 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Label } from './ui/label';
 import { Input } from './ui/input';
 import { cn } from '@/app/utils/cn';
 import { Textarea } from './ui/textarea';
 import { RadioGroup, RadioGroupItem } from '@/app/components/ui/radio-group';
 
+type Status = 'pending' | 'ok' | 'error';
+
 export function ContactForm() {
+  const [status, setStatus] = useState<Status | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      setStatus('pending');
+      setError(null);
+      const myForm = event.currentTarget;
+      const formData = new FormData(myForm);
+      const res = await fetch('/__forms.html', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(formData as any).toString(),
+      });
+      if (res.status === 200) {
+        setStatus('ok');
+      } else {
+        setStatus('error');
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      setStatus('error');
+      setError(`${e}`);
+    }
+  };
+
   return (
     <div className="md:border border-[var(--timberwolf)]  max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-[var(--night)] mb-8">
+      <div className="my-4 flex items-center justify-center">
+        {status === 'error' && (
+          <span className="text-red-700 text-center">
+            Something went wrong <br />
+            {error}
+          </span>
+        )}
+        {status === 'ok' && (
+          <span className="text-green-700 text-lg font-medium text-center">
+            Thank you for your submission
+          </span>
+        )}
+      </div>
       <h2 className="font-bold text-xl text-neutral-200">Join us</h2>
       <p className=" text-sm max-w-sm mt-2 text-neutral-300">
         Become a PLAYBACK venue partner.
       </p>
-
       <form
         className="my-8 flex flex-col items-start space-y-6"
-        data-netlify="true"
-        action="/contact"
         name="contact"
-        method="POST"
-        netlify-honeypot="bot-field"
+        onSubmit={handleFormSubmit}
       >
         <p className="hidden">
           <label>
@@ -30,7 +68,7 @@ export function ContactForm() {
           </label>
         </p>
         <input type="hidden" name="form-name" value="contact" />
-        <RadioGroup defaultValue="venue" className="mb-2">
+        <RadioGroup defaultValue="venue" className="mb-2" name="who">
           <Label className="mb-2">How am I?</Label>
           <div className="flex items-center space-x-2">
             <RadioGroupItem value="venue" id="r1" />
@@ -48,7 +86,7 @@ export function ContactForm() {
               id="firstname"
               placeholder="Tyler"
               type="text"
-              name="First Name"
+              name="first_name"
             />
           </LabelInputContainer>
           <LabelInputContainer>
@@ -57,7 +95,7 @@ export function ContactForm() {
               id="lastname"
               placeholder="Durden"
               type="text"
-              name="Last Name"
+              name="last_name"
             />
           </LabelInputContainer>
         </div>
@@ -67,7 +105,7 @@ export function ContactForm() {
             id="email"
             placeholder="projectmayhem@fc.com"
             type="email"
-            name="E-Mail"
+            name="email"
             required
           />
         </LabelInputContainer>
@@ -77,19 +115,20 @@ export function ContactForm() {
             id="company"
             placeholder="Alphabet LTD"
             type="text"
-            name="Company"
+            name="company"
             required
           />
         </LabelInputContainer>
         <LabelInputContainer>
           <Label htmlFor="message">Message (optional)</Label>
-          <Textarea id="message" placeholder="Hi..." name="Message" />
+          <Textarea id="message" placeholder="Hi..." name="message" />
         </LabelInputContainer>
         <button
           className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          disabled={status === 'pending'}
           type="submit"
         >
-          Send &rarr;
+          {status === 'pending' ? 'Loading...' : <>Send &rarr;</>}
           <BottomGradient />
         </button>
 
