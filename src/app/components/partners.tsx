c3cbdf443efb74d0a75a7ce467d5260e5d5b9ed2
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-06-17 00:40:05 +0100

    switch space to gap for logo spacing

--- src/app/components/partners.tsx
+++ src/app/components/partners.tsx
@@ -5,7 +5,7 @@ export const Partners = () => {
     return (
         <section className='container my-5'>
             <SectionTitle title='Our Partners' />
-            <div className='flex flex-wrap space-x-5 justify-center'>
+            <div className='flex flex-wrap gap-10 justify-center'>
                 <Image alt="SE FA Logo" src="/partners/soccerelite.svg" height={100} width={100} className='opacity-75 md:w-28' />
                 <Image alt="Forbes Logo" src="/partners/forbes.svg" height={100} width={100} className='opacity-75 md:w-28' />
                 {/* <Image alt="FC Urban Logo" src="/partners/fcurban.svg" height={100} width={100} className='opacity-75 md:w-28' /> */}
