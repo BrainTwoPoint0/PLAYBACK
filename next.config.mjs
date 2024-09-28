b538baea6b813a90bdf6357d9b406d61615f442c
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-09-28 20:38:24 +0100

    delete external image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [
-      'media.licdn.com',
-      'images.unsplash.com',
-      'en.reformsports.com',
-      'soccerelitefa.com',
-      'www.gsetiler.com',
-      'assets.aceternity.com'
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
