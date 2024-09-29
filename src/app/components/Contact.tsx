67dec29b4ab7807069f9bfb195697cb916b36dfc
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-09-29 16:44:29 +0100

    hide button when submission is successful

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -99,15 +99,16 @@ export function ContactForm() {
           <Label htmlFor="message">Message (optional)</Label>
           <Textarea id="message" placeholder="Hi..." name="message" />
         </LabelInputContainer>
-        <button
-          className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          disabled={status === 'pending'}
-          type="submit"
-        >
-          {status === 'pending' ? 'Loading...' : <>Send &rarr;</>}
-          <BottomGradient />
-        </button>
-
+        {status !== 'ok' && (
+          <button
+            className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            disabled={status === 'pending'}
+            type="submit"
+          >
+            {status === 'pending' ? 'Loading...' : <>Send &rarr;</>}
+            <BottomGradient />
+          </button>
+        )}
         <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1.5px] w-full" />
         {status !== null && (
           <div className="flex items-center justify-center w-full pt-2 text-lg font-medium text-center">
