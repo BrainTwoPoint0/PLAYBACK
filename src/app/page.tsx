ce7c74ec48e812a999866359543fa2b6db91fc70
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-10-17 15:20:22 -0400

    add flipping text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import { ParallaxText } from './components/ParallaxText';
 import { Header } from './components/Header';
 import { Partners } from './components/Partners';
-import { Ambassadors } from './components/Ambassadors';
 import { About } from './components/About';
 import { SportsList } from './components/SportsList';
 import Services from './components/Services';
+import { FlipWords } from './components/ui/flip-words';
+import Image from 'next/image';
 
 export default function Home() {
+  const words = ['PLAYERS', 'AGENTS', 'CLUBS', 'ACADEMIES', 'SCHOLARSHIPS', 'LEAGUES', 'TOURNAMENTS'];
   return (
     <main className="overflow-hidden">
       <Header />
@@ -18,8 +20,14 @@ export default function Home() {
       </div>
       <Partners />
       <Services />
+      <section className="md:container mt-36 text-xs md:text-4xl flex items-center md:justify-evenly justify-center">
+        <Image className='md:w-1/2 w-2/5' width={500} height={500} src={'/branding/PLAYBACK-Text.png'} alt='PLAYBACK Logo' />
+        <div className="md:w-2/5 w-1/2 pl-2 md:pl-0">
+          AN ECOSYSTEM <br />
+          TO POWER<FlipWords words={words} />
+        </div>
+      </section>
       <SportsList />
-      {/* <Ambassadors /> */}
     </main>
   );
 }
