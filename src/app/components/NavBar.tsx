cd6178060650b4cc7a82bf39cb3b7837acb12d24
Karim Fawaz <karimhilalfawaz@gmail.com>
2025-02-08 18:43:22 +0000

    update nav options

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -15,8 +15,9 @@ import Image from 'next/image';
 
 const navItems = [
   { href: '/academy', label: 'Academy' },
-  { href: '/leagues', label: 'Leagues', disabled: true },
-  { href: '/events', label: 'Events', disabled: true },
+  { href: '/league', label: 'League', disabled: true },
+  { href: '/venue', label: 'Venues', disabled: true },
+  { href: '/event', label: 'Events', disabled: true },
   { href: '/players', label: 'Players', disabled: true },
   { href: '/foundation', label: 'Foundation', disabled: true },
 ];
