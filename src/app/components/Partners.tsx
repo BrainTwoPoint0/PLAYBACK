6accfd71070ceb6549b641e11285690e515abb0f
Karim Fawaz <karimhilalfawaz@gmail.com>
2025-02-08 17:26:23 +0000

    add three logo

--- src/app/components/Partners.tsx
+++ src/app/components/Partners.tsx
@@ -49,8 +49,8 @@ export const Partners = () => {
           className="opacity-75 md:w-28"
         />
         <Image
-          alt="Galatasaray Logo"
-          src="/partners/galatasaray.svg"
+          alt="Three Logo"
+          src="/partners/three.svg"
           height={100}
           width={100}
           className="opacity-75 md:w-28"
