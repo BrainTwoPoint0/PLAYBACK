61b4635ae2c41b8759b68920f116d283d71a9388
Karim Fawaz <karimhilalfawaz@gmail.com>
2025-03-08 04:28:02 +0000

    fix partners logo

--- src/app/components/Partners.tsx
+++ src/app/components/Partners.tsx
@@ -11,77 +11,77 @@ export const Partners = () => {
           src="/partners/maestros.png"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="Forbes Logo"
           src="/partners/forbes.svg"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="Maidstone United Logo"
           src="/partners/maidstone.svg"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="Soccer Elite FA Logo"
           src="/partners/soccerelite.svg"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="City Group Logo"
           src="/partners/city.svg"
           height={100}
           width={100}
-          className="opacity-75 md:w-32"
+          className="opacity-75 md:w-32 md:h-32"
         />
         <Image
           alt="GOALS Logo"
           src="/partners/goals-logo.svg"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="DAFL Logo"
           src="/partners/dafl.png"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="Complete Football Academy Logo"
           src="/partners/cfa.png"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="Three Logo"
           src="/partners/three.svg"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="Star Meadow Logo"
           src="/partners/smsc.svg"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
         <Image
           alt="Hollands & Blair Logo"
           src="/partners/hb.png"
           height={100}
           width={100}
-          className="opacity-75 md:w-28"
+          className="opacity-75 md:w-28 md:h-28"
         />
       </div>
     </section>
