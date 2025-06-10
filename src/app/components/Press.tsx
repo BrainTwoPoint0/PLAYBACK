e50516021b58a8954712cd282333f028ce6faf3c
Karim Fawaz <karimhilalfawaz@gmail.com>
2025-06-11 00:14:12 +0100

    add GBEA

--- src/app/components/Press.tsx
+++ src/app/components/Press.tsx
@@ -4,27 +4,35 @@ export default function Press() {
   return (
     <section className="container mt-36">
       <SectionTitle title="Global Recognition" />
-      <div className="flex flex-wrap gap-x-10 md:gap-x-64 justify-center items-center">
+      <div className="flex flex-wrap gap-x-10 md:gap-x-40 justify-center items-center">
         <Image
           alt="WebSummit Logo"
           src="/media/websummit.png"
           height={100}
           width={100}
-          className="opacity-75 md:w-32 md:h-32"
+          className="md:w-32 md:h-32"
+        />
+
+        <Image
+          alt="Great British Entrepreneur Awards Logo"
+          src="/media/GBEA.svg"
+          height={100}
+          width={100}
+          className="md:w-40 md:h-40"
         />
         <Image
           alt="StartUp Awards Logo"
           src="/media/startup-awards.png"
           height={100}
           width={100}
-          className="opacity-75 md:w-32 md:h-32"
+          className="md:w-32 md:h-32"
         />
         <Image
           alt="UmmaHub Logo"
           src="/media/ummahub.png"
           height={100}
           width={100}
-          className="opacity-75 md:w-32 md:h-32"
+          className="md:w-32 md:h-32"
         />
       </div>
     </section>
